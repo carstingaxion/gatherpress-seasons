5ff99f197841931f56fabceb671bf6763be484bb
carstingaxion <carstingaxion@users.noreply.github.com>
2026-06-08 15:32:28 +0000

    Update version-numbers to 0.2.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GatherPress Seasons
  * Plugin URI:        https://github.com/carstingaxion/gatherpress-seasons
  * Description:
- * Version:           0.1.0
+ * Version:           0.2.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Requires plugins:  gatherpress
